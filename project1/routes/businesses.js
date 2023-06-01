57173c46e2fa61d118d2e5ed4071531c4b22feba
Melissa Swearingen <swearingenmj@gmail.com>
2023-05-31 17:26:40 -0700

    fix errors, pass tests

--- project1/routes/businesses.js
+++ project1/routes/businesses.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
+const businesses = [];
+const reviews = [];
+const photos = [];
+
 // POST /businesses
 router.post('/', jsonParser, function(req, res, next) {
   const { name, address, city, state, zip, phone, category, subcategories, website, email } = req.body;
@@ -25,6 +29,8 @@ router.post('/', jsonParser, function(req, res, next) {
     website,
     email
   }
+
+  businesses.push(newBusiness);
 
   res.status(201).json(newBusiness);
 });
@@ -55,11 +61,18 @@ router.get('/', function(req, res, next) {
         "next_page": `/businesses?page=${next_page}`,
         "prev_page": `/businesses?page=${prev_page}`,
       }
+
     } else {
-      res.status(404).json({ error: 'Page not found' });
-      return;
+      res.status(204).json({ error: 'Page not found' });
     }
-  } 
+  } else {
+    page_data = {
+      "page_number": "1",
+      "total_pages": "1",
+      "next_page": `/businesses?page=1`,
+      "prev_page": null,
+    }
+  }
 
   res.status(200).json(page_data);
 });
@@ -67,17 +80,15 @@ router.get('/', function(req, res, next) {
 // GET /businesses/:businessId
 router.get('/:businessId', function(req, res, next) {
   const id = req.params.businessId;
-  const business_data = [business_data[id]];
+  const business_data = [businesses[id]];
 
   if (id >= 0 && id <= businesses.length) {
     // get reviews
-    const reviews = reviews.filter(review => review.business_id == id);
     for (let i = 0; i < reviews.length; i++) {
       business_data.push(reviews[i]);
     }
 
     // get photos
-    const photos = photos.filter(photo => photo.business_id == id);
     for (let i = 0; i < photos.length; i++) {
       business_data.push(photos[i]);
     }
@@ -92,7 +103,7 @@ router.get('/:businessId', function(req, res, next) {
 // DELETE /businesses/:businessId
 router.delete('/:businessId', function(req, res, next) {
   const id = req.params.businessId;
-  business_data.splice(id, 1);
+  businesses.splice(id, 1);
   res.status(200).send('Business deleted successfully.');
 });
 
