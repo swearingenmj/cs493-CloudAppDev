37299f3e47ccc125e66eaa8109d9d802349a7782
Melissa Swearingen <swearingenmj@gmail.com>
2023-05-31 17:27:34 -0700

    fix errors, pass tests

--- project1/routes/reviews.js
+++ project1/routes/reviews.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// GET /users/:userId/reviews
-router.get('/:userId/reviews', function(req, res, next) {
-    res.status(200).send('GET /users/:userId/reviews');
+// GET /reviews
+router.get('/', function(req, res, next) {
+    res.status(200).send('GET /reviews');
 });
 
 module.exports = router;
